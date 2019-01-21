a1561b2ebd4906906ad0f8f688db604ea66caffb
Sam Mason <smason@cloudflare.com>
2019-01-21 11:16:32 +0000

    Fix file name case change

---
+++ www/src/pages/Api.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Box, Heading } from "rebass"
+
+const Api = () => {
+  return (
+    <Box>
+      <Heading>Coming Soon!</Heading>
+    </Box>
+  )
+}
+
+export default Api
