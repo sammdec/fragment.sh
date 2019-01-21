a8d8ae1266f2f1729ed089aa1b084616795d46e2
Sam Mason <smason@cloudflare.com>
2019-01-21 16:49:54 +0000

    Add titles

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -8,15 +8,19 @@ import Api from "./Api"
 export default createSwitch({
   paths: {
     "/": createPage({
+      title: "fragment.sh",
       getContent: () => <Home />
     }),
     "/reveal": createPage({
+      title: "fragment.sh - reveal your secret",
       getContent: () => <Reveal />
     }),
     "/faq": createPage({
+      title: "fragment.sh - faq",
       getContent: () => <FAQ />
     }),
     "/api": createPage({
+      title: "fragment.sh - api",
       getContent: () => <Api />
     })
   }
